630742fde3a4fdccd873f1eacb1bbeed6e50c0a0
danielgonzagat <danielgonzagatj@gmail.com>
2025-08-20 16:59:03 -0300

    monorepo: add autopoietic-nets

---
+++ autopoietic-nets/README.md
@@ -0,0 +1,90 @@
+# Autopoietic Networks
+
+A type of cellular automaton with the intent of simulating [autopoiesis](https://en.wikipedia.org/wiki/Autopoiesis) in an emergent manner, with a simple scaling rule and with each unit having binary state attribution and a corresponding gate associated to it. Such concept of autopoiesis is as introduced by [Maturana](https://en.wikipedia.org/wiki/Humberto_Maturana) and [Varela](https://en.wikipedia.org/wiki/Francisco_Varela) in [Autopoiesis and Cognition: The Realization of the Living](https://doi.org/10.1007/978-94-009-8947-4), and also a bit developed in Principles of biological autonomy(1979) by Varela.
+
+Autopoiesis can be viewed as the capacity of a system to generate and (re)build itself. Associated to this concept of autopoiesis is that of [organizational closure](https://doi.org/10.1016/j.jtbi.2015.02.029) or closure of constraints. These very briefly mean that any process (and respective constraints on it) needs to generate atleast another constraint. Here, processes and relations between them evolve in order to maintain such closure. Through this lens, any new adaptation or behaviour taken by an organism can be seen as a compensation to a pertubation (e.g. self-replication).
+
+Alternatively to this emergent approach, some examples might be those of [Varela's model for the simulation of autopoiesis](https://doi.org/10.1016/0303-2647(74)90031-8), [Terrence Deacon's simulation for his autogen](https://pure.hva.nl/ws/portalfiles/portal/1564681/Deacon_Exploring_Constraint.pdf), and more generally approaches which employ object calculi, for example using λ-calculus, [Fontana's & Buss' abstract chemistry(although not directly applied to autopoiesis)](https://scholar.harvard.edu/files/walterfontana/files/arrival.pdf) being an example of that.
+
+### Details of the network
+
+A network with `N x N` units is initialized, with each unit's state $∈ \{0, 1\}$ being randomly assigned. Over each iteration (with a max of `N_iter`), there's a logic gate randomly chosen from a set (examples with `AND`, `OR` and `XOR`), for the update of each unit's state according to its neighbors' states.
+
+If a unit maintains its state after an iteration, regardless of its neighbors, its $\Phi$ value (which is supposed to mimick closure) increases by 1. Moreover, if the states respective to consecutive iterations are different, $\Phi$ is reset to 0. If $\Phi \geq \epsilon$, then the neighbors take the value of the main unit's state, and an ensemble is formed. With regard to this, $\epsilon$ can be fixed (`fix = True`) or evolve over time (`fix = False`) with $ε = \textrm{int}\left(\frac{\textrm{iter}(1 - \textrm{H}(S))}{k}\right)$, where $\textrm{iter}$ is the number of iterations chosen, $\textrm{H}(S)$ the entropy of the net, and $k$ being an assigned integer. Additionally, there are two other options: `geq_cond = True` for $\Phi \geq \epsilon$ and `geq_cond = False` for $\Phi = \epsilon$.
+
+There are multiple variants, such that [main2.py](https://github.com/gbragafibra/autopoietic-nets/blob/main/main2.py) generates the most interesting behaviour, it being applied with an euclidean neighborhood.
+
+### Interesting behaviour (some examples)
+
+It is worth noting that the aim is to get behaviour increasingly closer to being characterized as autopoietic, and that these examples are far from such.
+
+![](autopoietic_complex11/complex4.gif)
+
+![](autopoietic_complex11/complex5.gif)
+
+![](autopoietic_complex11/complex7.gif)
+
+![](autopoietic_complex11/complex8.gif)
+
+![](autopoietic_complex11/complex13.gif)
+
+![](autopoietic_complex11/complex15.gif)
+
+![](autopoietic_complex11/complex16.gif)
+
+![](autopoietic_complex11/complex17.gif)
+
+![](autopoietic_complex11/complex21.gif)
+
+![](autopoietic_complex11/complex22.gif)
+
+![](autopoietic_complex8/complex13.gif)
+
+![](autopoietic_complex8/complex14.gif)
+
+![](autopoietic_complex8/complex15.gif)
+
+![](autopoietic_complex8/complex16.gif)
+
+![](autopoietic_complex8/complex19.gif)
+
+![](autopoietic_complex8/complex20.gif)
+
+![](autopoietic_complex6/complex19.gif)
+
+![](autopoietic_complex6/complex3.gif)
+
+![](autopoietic_complex6/complex10.gif)
+
+![](autopoietic_complex6/complex12.gif)
+
+![](autopoietic_complex6/complex25.gif)
+
+![](autopoietic_complex6/complex25.gif)
+
+![](autopoietic_complex8/complex3.gif)
+
+![](autopoietic_complex8/complex11.gif)
+
+![](autopoietic_complex8/complex12.gif)
+
+![](autopoietic_complex9/complex1.gif)
+
+![](autopoietic_complex9/complex2.gif)
+
+![](autopoietic_complex9/complex3.gif)
+
+![](autopoietic_complex9/complex4.gif)
+
+![](autopoietic_complex9/complex5.gif)
+
+![](autopoietic_complex9/complex6.gif)
+
+![](autopoietic_complex9/complex7.gif)
+
+
+
+
+
+
+
---
+++ autopoietic-nets/main.py
@@ -0,0 +1,151 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from gates import *
+
+gates = [AND, OR, XOR] #more interesting behaviour without neg gates
+
+#Compute entropy of the net
+def H(S):
+	# S: state matrix
+    counts = np.unique(S, return_counts=True)[1]
+    p = counts/(N**2)
+    return -np.sum(p * np.log(p))
+
+#----------------------------
+"""
+Params and conditions
+"""
+N = 500 #neuron count -> N² neurons generated
+N_iter = 20 #number of iterations
+S = np.random.choice((0,1), size = (N, N)) #init state
+fix = True #to have ε fixed
+ε_fixed = 2#if ε fixed
+k = 8 #If not fixed -> used denominator -> At max ε -> N_iter/k
+Φ = np.zeros((N, N), dtype=int) #To keep track of synchronization at each neuron/ensemble
+extended = True #Considering also diagonal neighbors
+
+"""
+compare condition between Φ and ε
+If True -> Ensemble when Φ >= ε
+If False -> Ensemble when Φ == ε
+"""
+geq_cond = True 
+#----------------------------
+
+
+# Dynamics for ε
+def dynamics(*args, fixed = False):
+    if fixed: #fixed assignment of ε
+        return ε_fixed
+    else:
+        return int((N_iter * (1 - H(S)))/k)
+
+ε = dynamics(fixed = fix) #init ε
+        
+
+"""
+Init plot
+"""
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+mat1 = ax1.imshow(S, cmap="gray", vmin=0, vmax=1)
+mat2 = ax2.imshow(Φ, cmap="hot", vmin=0, vmax=N_iter)
+ax1.set_title(f"Threshold (ε): {ε}")
+ax1.axis("off")
+ax2.set_title("Φ")
+ax2.axis("off")
+cbar2 = fig.colorbar(mat2, ax=ax2)
+cbar2.set_label("Synchronization Count (Φ)")
+
+
+#if to have individual gate assignment for each neuron
+#only at start doesn't update over iterations
+gate = np.random.choice(gates, (N, N))
+
+def update(frame, *args):
+    global S, Φ, ε, gate
+
+    print(f"Iteration {frame + 1}/{N_iter}")
+
+    # reallocate gate choice again (local-wise)
+    # if synchronization count too high
+    if np.mean(Φ) >= ε:
+        gate = np.random.choice(gates, (N, N))
+
+    #------
+    #choose a gate randomly for each iteration (globally)
+    #gate = np.random.choice(gates) 
+    #------
+    if extended:
+        #Getting neighbors by shifting matrix
+        left = np.roll(S, -1, axis = 1)
+        right = np.roll(S, 1, axis = 1)
+        up = np.roll(S, 1, axis = 0)
+        down = np.roll(S, -1, axis = 0)
+        upper_left = np.roll(np.roll(S, 1, axis = 0), -1, axis = 1)
+        upper_right = np.roll(np.roll(S, 1, axis = 0), 1, axis = 1)
+        bottom_left = np.roll(np.roll(S, -1, axis = 0), -1, axis = 1)
+        bottom_right = np.roll(np.roll(S, -1, axis = 0), 1, axis = 1)
+
+        # stack neighbor states
+        neighbors = np.stack([left, right, up, down, upper_left,
+         upper_right, bottom_left, bottom_right], axis = 0)
+
+
+    else:
+        #Getting neighbors by shifting matrix
+        left = np.roll(S, -1, axis = 1)
+        right = np.roll(S, 1, axis = 1)
+        up = np.roll(S, 1, axis = 0)
+        down = np.roll(S, -1, axis = 0)
+
+        # stack neighbor states
+        neighbors = np.stack([left, right, up, down], axis = 0)
+
+    #return new state
+    #new_state = gate(neighbors) #if for global gate assignment
+
+    #for individual assignment of gates
+    new_state = np.array([[gate[i, j](neighbors[:, i, j]) for j in range(N)] for i in range(N)])
+
+
+    sync = (new_state == S)
+    Φ[sync] += 1 
+    Φ[~sync] = 0
+
+    if geq_cond:
+        mask_ensemble = (Φ >= ε)
+    else:
+        mask_ensemble = (Φ == ε)
+    ε = dynamics(fixed = fix)
+    S = new_state #update state
+    # update given any ensemble formation
+    if np.any(mask_ensemble):
+        ensemble_idxs = np.argwhere(mask_ensemble)
+        for i, j in ensemble_idxs:
+        	#update neighbors given central neuron forming ensemble
+            if extended: #all neighbors(including diagonals)
+                S[(i-1) % N, (j-1) % N] = S[i, j]  #upper left 
+                S[(i-1) % N, (j+1) % N] = S[i, j]  #upper right 
+                S[(i+1) % N, (j+1) % N] = S[i, j]  #bottom right 
+                S[(i+1) % N, (j-1) % N] = S[i, j]  #bottom left
+                S[i, (j-1) % N] = S[i, j]  #left 
+                S[i, (j+1) % N] = S[i, j]  #right 
+                S[(i-1) % N, j] = S[i, j]  #up 
+                S[(i+1) % N, j] = S[i, j]  #down 
+                
+            else: 
+                S[i, (j-1) % N] = S[i, j]  #left 
+                S[i, (j+1) % N] = S[i, j]  #right 
+                S[(i-1) % N, j] = S[i, j]  #up 
+                S[(i+1) % N, j] = S[i, j]  #down 
+    mat1.set_array(S)
+    mat2.set_array(Φ)
+
+    ax1.set_title(f"Threshold (ε): {ε}; Fixed (k: {k if not fix else None}): {fix}; Φ $\geq$ ε: {geq_cond}; Extended: {extended}")
+    return mat1, mat2
+
+ani = FuncAnimation(fig, update, frames=N_iter, interval=1000)
+ani.save("autopoietic_net.gif", writer="pillow", fps=10)
+print("Finished!")
+#plt.show()
